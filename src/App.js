53557516b1e9ef726413af1815467f32f4831969
Teo Si-Yan <teosiyan@hotmail.com>
2018-07-11 15:05:15 +0800

    UI for uploading image and question

--- src/App.js
+++ src/App.js
@@ -3,16 +3,42 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    img: '',
+    imgUrl: '',
+    question: '',
+  }
+
+  previewImage(e) {
+    let reader = new FileReader();
+    let file = e.target.files[0];
+
+    reader.onloadend = () => {
+      this.setState({
+        img: file,
+        imgUrl: reader.result
+      });
+    }
+
+    reader.readAsDataURL(file)
+  }
+
+  handleSubmit(e) {
+    console.log('submitting')
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h4>1. Upload an image</h4>
+        <input type="file" onChange={this.previewImage.bind(this)} /><br /><br />
+        <img src={this.state.imgUrl} style={{maxWidth: '400px', maxHeight: '400px'}} />
+
+        <h4>2. Ask a question</h4>
+        <input style={{width: '400px'}} type="text" placeholder="Enter a question" value={this.state.question} onChange={e => this.setState({question: e.target.value})} />
+
+        <br /><br />
+        <button onClick={this.handleSubmit.bind(this)}>Submit</button>
       </div>
     );
   }
