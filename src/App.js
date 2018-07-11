f496db396a966565cf351cadc5f554ce8c725147
Teo Si-Yan <teosiyan@hotmail.com>
2018-07-11 17:03:45 +0800

    Integrate with api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import request from 'superagent'
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,6 +8,8 @@ class App extends Component {
     img: '',
     imgUrl: '',
     question: '',
+    isLoading: false,
+    answers: [],
   }
 
   previewImage(e) {
@@ -25,6 +28,21 @@ class App extends Component {
 
   handleSubmit(e) {
     console.log('submitting')
+    console.log(this.state.img)
+    this.setState({ isLoading: true })
+
+    request
+      .post('http://ec2-34-220-163-4.us-west-2.compute.amazonaws.com/')
+      .attach('image', this.state.img)
+      .field('question', this.state.question)
+      .then((res) => {
+        console.log(res)
+        this.setState({ answers: res.body.pred, isLoading:false })
+      })
+      .catch((err) => {
+        this.setState({ isLoading:false })
+        console.log(err)
+      })
   }
 
   render() {
@@ -39,6 +57,15 @@ class App extends Component {
 
         <br /><br />
         <button onClick={this.handleSubmit.bind(this)}>Submit</button>
+        
+        {this.state.isLoading && <p>Computing...</p>}
+        {this.state.answers.length>0 && <h4>Predicted answers:</h4>}
+        {this.state.answers.map((v, k) =>
+          <div key={k}>
+            {v[0]}
+            <div className="pbar" style={{width: Math.round(v[1]*400)+'px'}}>{v[1]*100}%</div>
+          </div>
+        )}
       </div>
     );
   }
